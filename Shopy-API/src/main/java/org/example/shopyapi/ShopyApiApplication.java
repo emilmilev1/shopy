99e840b00e24710f332b1220e9515d69ccc72003
Emil Milev <emo.2012012@gmail.com>
2025-06-20 09:28:24 +0300

    Add main file

---
+++ Shopy-API/src/main/java/org/example/shopyapi/ShopyApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.shopyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopyApiApplication.class, args);
+    }
+
+}
